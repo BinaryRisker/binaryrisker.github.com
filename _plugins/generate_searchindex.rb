332c5fe853083a84a980fecb26883bc4ef880687
BinaryRisker <jianglianhui2008@126.com>
2015-06-14 12:15:09 +0800

    modified config and used fast-stemmer instead of lingua/stemmer

--- _plugins/generate_searchindex.rb
+++ _plugins/generate_searchindex.rb
@@ -27,35 +27,35 @@ module Jekyll
       self.data['description'] = post.data['description']
     end
   end
-	
+
 	class Site
-	
+
 		attr_accessor :search_index, :search_posts
-  
+
 		def write_search_files
 
 			createindex!
-  	
+
 			dir = self.config['search_dir'] || 'search'
-			
+
 			self.search_index.keys.each do |letter|
 				write_search_index(self, File.join(dir, 'terms'), letter, self.search_index[letter])
 			end
-						
+
 			self.search_posts.keys.each do |i|
 				write_search_post(self, File.join(dir, 'posts'), i, self.search_posts[i])
 			end
-  	
+
 		end
-  
+
     def write_search_index(site, dir, letter, data)
     	require 'json'
-      index = SearchIndex.new(site, site.source, dir, letter)        			
+      index = SearchIndex.new(site, site.source, dir, letter)
 			index.output = data.to_json
       index.write(site.dest)
       self.static_files << index
     end
-    
+
     def write_search_post(site, dir, pid, post)
       index = SearchPost.new(site, site.source, dir, pid, post)
       index.render(site.layouts, site_payload)
@@ -63,65 +63,65 @@ module Jekyll
       # Record the fact that this page has been added, otherwise Site::cleanup will remove it.
       self.static_files << index
     end
-    
+
     def createindex!
-        
-		require 'lingua/stemmer'
-		stemmer = Lingua::Stemmer.new
-		
+
+		require 'fast_stemmer'
+		#stemmer = Lingua::Stemmer.new
+
 		searchwords = Hash.new
 		postlist = Hash.new
-					
+
       self.posts.each_index do |i|
-      
+
       	rawtext = self.posts[i].to_s.downcase
 
       	if self.posts[i].data['title']
       		rawtext << ' ' + self.posts[i].data['title'].downcase
       	end
-      	
+
       	if self.posts[i].data['description']
       		rawtext << ' ' + self.posts[i].data['description'].downcase
       	end
-			
+
 			rawtext.scan(/[a-zA-Z0-9]{1,}/).each do |word|
-			
+
 				postlist[i] = self.posts[i]
-			
-				word = stemmer.stem(word)
+
+				word = Stemmer::stem_word(word)
 				letter = word[0,2]
-				
+
 				if !searchwords.key?(letter)
 					searchwords[letter] = Hash.new
 				end
-				
+
 				if !searchwords[letter].key?(word)
 					searchwords[letter][word] = Array.new
 				end
-				
+
 				searchwords[letter][word].push(i)
-				
+
 			end
-				
+
       end
 
 		self.search_index = searchwords
 		self.search_posts = postlist
 
     end
-    
+
   end
-  
-    
+
+
   class SearchGenerator < Generator
 
 		safe true
 		priority :low
-				
+
 		def generate(site)
-			site.write_search_files if (site.config['searchindex']) 
+			site.write_search_files if (site.config['searchindex'])
 		end
-    	
+
 	end
 
 end
